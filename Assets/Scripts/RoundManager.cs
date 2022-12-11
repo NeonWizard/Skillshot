e3c3740d7b917162806a9f8154c4426c0c1c86f2
Wes <averagewizard13@gmail.com>
2022-12-10 23:57:27 -0800

    Add ring spawning

--- Assets/Scripts/RoundManager.cs
+++ Assets/Scripts/RoundManager.cs
@@ -24,8 +24,14 @@ public class RoundManager : MonoBehaviour {
     public OverlayController overlayController;
     public AudioSource backgroundMusic;
 
+    [Header("Prefabs")]
+    public GameObject ringPrefab;
+
     [Header("Variables")]
     public int roundTime = 30;
+    public BoxCollider ringSpawnArea;
+
+    private List<GameObject> rings = new List<GameObject>();
 
     void Awake() {
         _instance = this;
@@ -83,6 +89,37 @@ public class RoundManager : MonoBehaviour {
     }
 
     public void SpawnRings() {
+        // Delete old rings
+        foreach (GameObject ring in rings) {
+            Destroy(ring);
+        }
+        rings.Clear();
 
+        // Spawn new rings
+        int numToSpawn = (int)Mathf.Min(Mathf.Floor(Random.Range(2, 4) + Random.Range(0, CurrentRound)), 8);
+        for (int i = 0; i < numToSpawn; i++) {
+            GameObject ring = Instantiate(ringPrefab);
+
+            Vector3 colliderPoint = GetRandomPointInsideCollider(ringSpawnArea);
+            ring.transform.position = ringSpawnArea.transform.TransformPoint(colliderPoint);
+            if (colliderPoint.x > 0) {
+                Vector3 oldScale = ring.transform.localScale;
+                oldScale.x = -1;
+                ring.transform.localScale = oldScale;
+            }
+
+            rings.Add(ring);
+        }
+    }
+
+    // todo: move to a utils static singleton or something
+    public static Vector3 GetRandomPointInsideCollider(BoxCollider boxCollider) {
+        Vector3 extents = boxCollider.size / 2f;
+        Vector3 point = new Vector3(
+            Random.Range(-extents.x, extents.x),
+            Random.Range(-extents.y, extents.y),
+            Random.Range(-extents.z, extents.z)
+        );
+        return point;
     }
 }
